9c83c779efb50190852295a373eab970aea3a2f1
Pooja Tewari <poojatewari410@gmail.com>
2018-12-03 01:01:20 -0500

    auth and re-routing completely works

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.render('index');
 });
+app.get('/dashboard', function(req, res) {
+  res.render('dashboard');
+});
 app.listen(port, function(){
   console.log('Node js Express js Tutorial');
 });
