2bf76be9830949cc8751cc006337bd306aff2275
Pooja Tewari <poojatewari410@gmail.com>
2018-12-02 23:51:42 -0500

    server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+app.listen(port, function(){
+  console.log('Node js Express js Tutorial');
+});
